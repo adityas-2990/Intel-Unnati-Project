6a45ebd2ec828b5a52f4bb0bc3697be2ac09bb4a
Aditya <adi.official.2990@gmail.com>
2024-07-03 22:16:49 +0530

    Added description at various places

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def KNN_missing_values(df , n_neighbors):
 
 # Main function
 def main():
-    st.title('Data Analysis App')
-    st.write('Welcome to the Data Analysis App!')
+    st.title('Data Analysis App 📊')
+    st.write('Welcome to the Data Analysis App! This app is designed to help you analyze your dataset. You can upload your dataset, handle missing values, visualize your data, and perform clustering on your dataset with the help of a few simple clicks. Let\'s get started!!!')
 
     # Load the dataset
     st.sidebar.header('Upload Data')
@@ -53,23 +53,33 @@ def main():
     if uploaded_file is not None:
         st.sidebar.write('File uploaded successfully!')
         df = pd.read_csv(uploaded_file)
+        st.markdown("""---""") 
+        st.subheader('Your Dataset: ')
         st.write(df.head())
 
         #Store the dataframe in session state
         if 'df' not in st.session_state:
             st.session_state.df = df
 
+        st.markdown("""---""") 
         # Display the dataset shape
+        st.subheader('Dataset Shape: ')
         st.write("The number of features in the dataset are: ", st.session_state.df.shape[1])
         st.write("The number of samples in the dataset are: ", st.session_state.df.shape[0])
+        st.markdown("""---""") 
 
         # Display the dataset columns
+        st.subheader('Dataset Columns: ')
         st.write('The dataset columns are: ', st.session_state.df.columns)
+        st.markdown("""---""") 
 
         # Display the dataset summary
+        st.subheader('Basic Dataset Summary: ')
         st.write('The dataset summary is: ', st.session_state.df.describe())
+        st.markdown("""---""") 
 
         # Display columns with missing values
+        st.subheader('Columns with Missing Values: ')
         missing_values = st.session_state.df.isnull().sum()
         st.write('The columns with missing values are: ')
         st.write(missing_values[missing_values>0])
@@ -81,8 +91,11 @@ def main():
         sns.heatmap(df_corr.isnull(), cbar=False)
         st.pyplot(fig)
 
+        st.markdown("""---""") 
+
         #Creating Buttons to choose the method of handling missing values
         st.sidebar.header('Handle Missing Values')
+        st.sidebar.write("Missing values can be dealt with by filling them with the mean, median, mode or by dropping the column. There is also an option to use KNN Imputer to handle missing values.")
         missing_values = df.isnull().sum()
         missing_columns = missing_values[missing_values > 0].index.tolist()
         
@@ -96,7 +109,7 @@ def main():
                 st.session_state.method = method
             
     
-            st.write(st.session_state.df.head())
+            #st.write(st.session_state.df.head())
             if st.sidebar.button("Handle Missing Values" , key='handle_missing_values'):
                 if method == 'Fill with Mean':
                     st.session_state.df[column].fillna(df[column].mean(), inplace=True)
@@ -118,7 +131,7 @@ def main():
         n_neighbors = st.sidebar.slider('Number of Neighbors', min_value=1, max_value=10)
         if st.sidebar.button("Handle Missing Values with KNN Imputer", key='handle_missing_values_knn'):
             df = KNN_missing_values(st.session_state.df, n_neighbors)
-            st.write('The number of missing values in the dataset after handling are: ', st.session_state.df.isnull().sum().sum())
+            #st.write('The number of missing values in the dataset after handling are: ', st.session_state.df.isnull().sum().sum())
             st.write('The dataset after handling missing values is: ', st.session_state.df.head())
         
 
@@ -128,18 +141,22 @@ def main():
             st.write(st.session_state.df.head())
 
 
+        st.sidebar.header('Data Visualization')
         #Drop down menu to select the categorical colums in the dataset
-        st.sidebar.header('Select Categorical Columns')
+        st.sidebar.subheader('Select Categorical Columns')
+        st.sidebar.write('Categories are the columns which have a fixed number of unique values. For example sex of a person can or type of a product can be termed as a category. The categories are plotted on the y-axis and the numerical columns are plotted on the x-axis. Only a bar graph can be plotted for categorical columns.')
         categorical_columns = st.sidebar.multiselect('Select Columns', options=st.session_state.df.columns.tolist())
         numerical_columns = st.session_state.df.select_dtypes(include=['int64', 'float64']).columns.tolist()
 
         #Selecting the columns for plotting(Categorical)
-        st.sidebar.header('For Categorical Columns')
+        st.sidebar.subheader('Graph for Categorical Columns')
+        st.sidebar.write('Select the x-axis and y-axis for plotting the bar graph. The x-axis consists of  numerical column and the y-axis consists of categorical columns selected.')
         x_feature_category = st.sidebar.selectbox('X-axis', options=numerical_columns , key='x_feature_category')
         y_feature_category = st.sidebar.selectbox('Y-axis(Categories)', options=categorical_columns , key='y_feature_category')
 
         #Bar Graph for Categorical Columns
         if st.sidebar.button('Plot Data' , key='plot_data_category'):
+            st.subheader('Plotting Area')
             if x_feature_category and y_feature_category:
                 fig = plt.figure()
                 sns.barplot(x=x_feature_category, y=y_feature_category, data=st.session_state.df)
@@ -147,13 +164,15 @@ def main():
 
 
         #Selecting the columns for plotting(Numerical)
-        st.sidebar.header('For Numerical Columns')
+        st.sidebar.subheader('Graph for Numerical Columns')
+        st.sidebar.write('Select the x-axis and y-axis for plotting the graph. The x-axis and y-axis consists of numerical columns selected. you can select the type of graph you want to plot.')
         x_feature_numeric = st.sidebar.selectbox('X-axis', options=numerical_columns , key='x_feature_numeric')
         y_feature_numeric = st.sidebar.selectbox('Y-axis', options=numerical_columns , key='y_feature_numeric')
         graph_type = st.sidebar.selectbox('Graph Type', options=['Scatter Plot', 'Line Plot', 'Bar Plot'])
 
         #Different Graphs for Numerical Columns
         if st.sidebar.button('Plot Data' , key='plot_data_numeric'):
+            st.subheader('Plotting Area')
             if x_feature_numeric and y_feature_numeric:
                 if graph_type == 'Scatter Plot':
                     st.write('Scatter Plot')
@@ -178,9 +197,11 @@ def main():
         
         #Plotting clusters in a column
         st.sidebar.header('Clustering')
+        st.sidebar.write('Clustering is the task of dividing the population or data points into a number of groups such that data points in the same groups are more similar to other data points in the same group than those in other groups. The number of clusters to be formed can be selected by the user.')
         cluster_column = st.sidebar.selectbox('Select Column', options=numerical_columns)
         n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10)
         if st.sidebar.button('Plot Clusters', key='plot_clusters'):
+            st.subheader('Clusters')
             if cluster_column:
                 from sklearn.cluster import KMeans
                 kmeans = KMeans(n_clusters=n_clusters)
